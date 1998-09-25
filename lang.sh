9323508a348b5804df65ce34d3731c46ca1bc1c0
Cristian Gafton <gafton@redhat.com>
1998-09-25 19:15:55 +0000

    Set INPUTRC and LESSCHARSET on linux-lat systems

--- lang.sh
+++ lang.sh
@@ -1,15 +1,32 @@
-if [ -f /etc/sysconfig/i18n ]; then
-        . /etc/sysconfig/i18n
+#!/bin/bash
 
-  if [ -n "$LANG" ]; then
-          export LANG
-  fi
+if [ -f /etc/sysconfig/i18n ]; then
+    . /etc/sysconfig/i18n
+
+    if [ -n "$LANG" ]; then
+	export LANG
+    fi
   
-  if [ -n "$LINGUAS" ]; then
-          export LINGUAS
-  fi
+    if [ -n "$LINGUAS" ]; then
+	export LINGUAS
+    fi
   
-  if [ -n "$SYSTERM" ]; then
-          export TERM=$SYSTERM
-  fi
+    if [ -n "$SYSTERM" ]; then
+	export TERM=$SYSTERM
+    fi
+
+    # Set console font map.
+    if [ -n "$UNIMAP" ]; then
+	loadunimap $UNIMAP
+    fi
+
+    if [ -n "$SYSTEM" ] ; then
+	case $SYSTEM in
+	    linux-lat)
+		LESSCHARSET=latin1
+		INPUTRC=/etc/inputrc
+		export LESSCHARSET INPUTRC
+		;;
+	esac
+    fi
 fi
