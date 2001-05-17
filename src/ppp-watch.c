92506822c1748782e57254282536f9f29e2e0799
Bill Nottingham <notting@redhat.com>
2001-05-17 19:55:16 +0000

    don't call ifup-ppp on sigint (#40585)

--- src/ppp-watch.c
+++ src/ppp-watch.c
@@ -740,6 +740,7 @@ main(int argc, char **argv) {
 		sleep(timeout);
 		sigprocmask(SIG_BLOCK, &blockedsigs, NULL);
 		if (!theSigterm &&
+		    !theSigint && 
 		    !theSighup &&
 		    !theSigio &&
 		    !theSigchld &&
