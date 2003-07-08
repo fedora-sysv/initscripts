9c51f7ea621bd202c58a44a61772acf97c8a48c0
Bill Nottingham <notting@redhat.com>
2003-07-08 20:15:34 +0000

    add a test to consoletype to test that we're the current foreground console

--- src/consoletype.c
+++ src/consoletype.c
@@ -7,21 +7,31 @@
 int main(int argc, char **argv)
 {
     unsigned char twelve = 12;
-    int maj;
+    char *type;
+    int maj, min, ret = 0, fg = -1;
     struct stat sb;
-
+	
     fstat(0, &sb);
     maj = major(sb.st_rdev);
+    min = minor(sb.st_rdev);
     if (maj != 3 && (maj < 136 || maj > 143)) {
-	if (ioctl (0, TIOCLINUX, &twelve) < 0) {
-	    printf ("serial\n");
-	    return 1;
+	if ((fg = ioctl (0, TIOCLINUX, &twelve)) < 0) {
+	    type = "serial";
+	    ret = 1;
 	} else {
-	    printf ("vt\n");
-	    return 0;
+	    type = "vt";
+	    ret = 0;
 	}
     } else {
-	printf ("pty\n");
-	return 2;
+	type = "pty";
+	ret = 2;
+    }
+    if (argc > 1 && !strcmp(argv[1],"fg")) {
+	    if (fg < 0 || fg != (min-1))
+		    return 1;
+	    return 0;
+    } else {
+	    printf("%s\n",type);
+	    return ret;
     }
 } 
