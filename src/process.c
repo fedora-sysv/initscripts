170aa264f196111dd4a644fe81f08a300566bcd1
Bill Nottingham <notting@redhat.com>
2005-03-31 21:39:24 +0000

    free some of the more egregious memory leaks (#85935)

--- src/process.c
+++ src/process.c
@@ -188,7 +188,8 @@ int monitor(char *cmdname, int pid, int numfds, int *fds, int reexec, int quiet,
 	     int bytesread = 0;
 	     
 	     do {
-		char *buf=calloc(8192,sizeof(char));
+		char *b, *buf=calloc(8192,sizeof(char));
+		b = buf;
 		bytesread = read(pfds[y].fd,buf,8192);
 		if (bytesread==-1) {
 		   perror("read");
@@ -251,11 +252,16 @@ int monitor(char *cmdname, int pid, int numfds, int *fds, int reexec, int quiet,
 			      }
 			      cmdargs[cmdargc+1]=NULL;
 			      processArgs(cmdargc+1,cmdargs,1);
+			      for (z=0;z<(cmdargc);z++) {
+				  free(cmdargs[z]);
+			      }
+			      free(cmdargs);
 			  }
 		      }
+		      if (tmpstr) free(tmpstr);
 		  }
 		}
-                free(buf);
+                free(b);
 	     } while ( bytesread==8192 );
 	  }
 	  y++;
