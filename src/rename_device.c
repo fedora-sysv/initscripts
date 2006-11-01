f53aaea85ead84e8d54a8adff1e8052c53247e88
Bill Nottingham <notting@redhat.com>
2006-11-01 21:23:12 +0000

    I can't count

--- src/rename_device.c
+++ src/rename_device.c
@@ -107,7 +107,7 @@ int isCfg(const struct dirent *dent) {
 		return 0;
 	if (dent->d_name[len-1] == '~')
 		return 0;
-	if (!strncmp(dent->d_name+len-5,".bak",4))
+	if (!strncmp(dent->d_name+len-4,".bak",4))
 		return 0;
 	return 1;
 }
