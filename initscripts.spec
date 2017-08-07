fedora-sysv/initscripts

commit 13d638c0a7df4a998429163b9a0189dcffa2393a

Drop sysctl.d/00-system.conf
This file now only contains lines to disable netfilter on bridges.
In kernel 3.18 this filtering functionality was made non-default by
moving it to br_netfilter [https://bugzilla.redhat.com/show_bug.cgi?id=512206].
Aybody who actually wants to use br_netfilter has to load it explicitly
anyway, so disabling it through sysctl isn't necessary anymore.

The reason for removal is that by default (i.e. when br_netfilter is
not loaded), we get a warning on every boot:

systemd-sysctl[210]: Couldn't write '0' to 'net/bridge/bridge-nf-call-ip6tables', ignoring: No such file or directory
systemd-sysctl[210]: Couldn't write '0' to 'net/bridge/bridge-nf-call-iptables', ignoring: No such file or directory
systemd-sysctl[210]: Couldn't write '0' to 'net/bridge/bridge-nf-call-arptables', ignoring: No such file or directory

This is confusing for users and inelegant.

The downside of removing this file is for people who load br_netfilter
for some reason *and* do no want to use it, will have to take an
additional step now (either restore the sysctl settings or remove br_netfilter
from /etc/modules-load.d or wherever). I expect the number of people affected
to be very small.

(Note that the file was overwritten on sparc and s390, so those
architectures see no change.)

initscripts.spec
@@ -143,7 +143,6 @@ fi
 %dir %{_sysconfdir}/rc.d/init.d
 %{_sysconfdir}/rc.d/init.d/*
 %ghost %verify(not md5 size mtime) %config(noreplace,missingok) %{_sysconfdir}/rc.d/rc.local
-%{_prefix}/lib/sysctl.d/00-system.conf
 %exclude %{_sysconfdir}/profile.d/debug*
 %{_sysconfdir}/profile.d/*
 %{_sbindir}/sys-unconfig

Makefile
@@ -25,13 +25,10 @@ install:
 	install -m644  debug.csh debug.sh $(ROOT)/etc/profile.d
 	install -m755  sys-unconfig $(ROOT)/usr/sbin
 	install -m644  service.8 sys-unconfig.8 $(ROOT)$(mandir)/man8
-	mkdir -p -m 755 $(ROOT)/usr/lib/sysctl.d
-	mkdir -p -m 755 $(ROOT)/etc/sysctl.d
-	install -m644 sysctl.conf $(ROOT)/usr/lib/sysctl.d/00-system.conf
 	if uname -m | grep -q sparc ; then \
-	  install -m644 sysctl.conf.sparc $(ROOT)/usr/lib/sysctl.d/00-system.conf ; fi
+	  install -D -m644 sysctl.conf.sparc $(ROOT)/usr/lib/sysctl.d/00-system.conf ; fi
 	if uname -m | grep -q s390 ; then \
-	  install -m644 sysctl.conf.s390 $(ROOT)/usr/lib/sysctl.d/00-system.conf ; fi
+	  install -D -m644 sysctl.conf.s390 $(ROOT)/usr/lib/sysctl.d/00-system.conf ; fi
 
 	install -m755 -d $(ROOT)/etc/rc.d $(ROOT)/etc/sysconfig
 	cp -af rc.d/init.d $(ROOT)/etc/rc.d/

sysctl.conf
@@ -1,9 +1,0 @@
-# Kernel sysctl configuration file
-#
-# For binary values, 0 is disabled, 1 is enabled.  See sysctl(8) and
-# sysctl.conf(5) for more details.
-
-# Disable netfilter on bridges.
-net.bridge.bridge-nf-call-ip6tables = 0
-net.bridge.bridge-nf-call-iptables = 0
-net.bridge.bridge-nf-call-arptables = 0

commit ed552513d41c16ba3e29d83e3134a88e509a78d8

specfile: Fix failing build for s390* architecture
  Error caused by commit 13d638c0a7df4a.

initscripts.spec
@@ -129,6 +129,7 @@ fi
 %{_sysconfdir}/sysconfig/network-scripts/ifdown-isdn
 %ifarch s390 s390x
 %{_sysconfdir}/sysconfig/network-scripts/ifup-ctc
+%{_prefix}/lib/sysctl.d/00-system.conf
 %endif
 %config(noreplace) %{_sysconfdir}/networks
 %config(noreplace) %{_sysconfdir}/rwtab