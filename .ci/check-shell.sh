d64a6fe5b61b31d9245d5ad528afca1a77e97f00
Jan Macku <jamacku@redhat.com>
2020-09-08 11:04:01 +0200

    Rework of shell ci
    Bug fixes:
    * ci now checks only shell scripts
    
    New features:
    * add ability to ignore specific warnings
    * ci test files containing shebang #!/bin/bash or #!/bin/sh
    * ci test files with .sh extension
    * add ability to use inline comments in list files
    * use of colors in output
    * make code cleaner

--- .ci/check-shell.sh
+++ .ci/check-shell.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Path is wrong because of travis
 . ./.ci/functions.sh
 
 # ------------ #
@@ -8,63 +9,84 @@
 
 # https://medium.com/@joey_9999/how-to-only-lint-files-a-git-pull-request-modifies-3f02254ec5e0
 # get names of files from PR (excluding deleted files)
-# TRAVIS_COMMIT_RANGE - HEAD of destination branch ... HEAD of PR branch
 git diff --name-only --diff-filter=db "${TRAVIS_COMMIT_RANGE}" > ../pr-changes.txt
 
 # Find modified shell scripts
-readarray list_of_changes < <(grep "^[^#]" ../pr-changes.txt)
+list_of_changes=()
+file_to_array "../pr-changes.txt" "list_of_changes" 0
+list_of_scripts=()
+file_to_array "./.ci/script-list.txt" "list_of_scripts" 1
+
+# Create list of scripts for testing
 list_of_changed_scripts=()
 for file in "${list_of_changes[@]}"; do
-  # https://stackoverflow.com/questions/19345872/how-to-remove-a-newline-from-a-string-in-bash
-  # Remove new line and add ./ at the beginning
-  is_it_script "$file" && list_of_changed_scripts+=("./${file//[$'\t\r\n ']}")
+  is_it_script "$file" "${list_of_scripts[@]}" && list_of_changed_scripts+=("./${file}") && continue
+  check_extension "$file" && list_of_changed_scripts+=("./${file}") && continue
+  check_shebang "$file" && list_of_changed_scripts+=("./${file}")
 done
 
-echo "Changed shell scripts:"
+# Get list of exceptions
+list_of_exceptions=()
+file_to_array "./.ci/exception-list.txt" "list_of_exceptions" 1
+string_of_exceptions=$(join_by , "${list_of_exceptions[@]}")
+
+echo -e "\n"
+echo ":::::::::::::::::::::"
+echo -e "::: ${WHITE}Shellcheck CI${NOCOLOR} :::"
+echo ":::::::::::::::::::::"
+echo -e "\n"
+
+echo -e "${WHITE}Changed shell scripts:${NOCOLOR}"
 echo "${list_of_changed_scripts[@]}"
-echo "------------"
+echo -e "${WHITE}List of shellcheck exceptions:${NOCOLOR}"
+echo "${string_of_exceptions}"
+echo -e "\n"
 
 # ------------ #
 #  SHELLCHECK  #
 # ------------ #
 
 # sed part is to edit shellcheck output so csdiff/csgrep knows it is shellcheck output (--format=gcc)
-shellcheck --format=gcc "${list_of_changed_scripts[@]}" | sed -e 's|$| <--[shellcheck]|' > ../pr-br-shellcheck.err
+shellcheck --format=gcc --exclude="${string_of_exceptions}" "${list_of_changed_scripts[@]}" 2> /dev/null | sed -e 's|$| <--[shellcheck]|' > ../pr-br-shellcheck.err
 
 # make destination branch
-[[ ${TRAVIS_COMMIT_RANGE} =~ ^([0-9|a-f]*?)\. ]] && git checkout -b ci_br_dest "${BASH_REMATCH[1]}"
+[[ ${TRAVIS_COMMIT_RANGE} =~ ^([0-9|a-f]*?)\. ]] && git checkout -q -b ci_br_dest "${BASH_REMATCH[1]}"
 
-shellcheck --format=gcc "${list_of_changed_scripts[@]}" | sed -e 's|$| <--[shellcheck]|' > ../dest-br-shellcheck.err
+shellcheck --format=gcc --exclude="${string_of_exceptions}" "${list_of_changed_scripts[@]}" 2> /dev/null | sed -e 's|$| <--[shellcheck]|' > ../dest-br-shellcheck.err
 
 # ------------ #
 #  VALIDATION  #
 # ------------ #
 
 exitstatus=0
+echo ":::::::::::::::::::::::::"
+echo -e "::: ${WHITE}Validation Output${NOCOLOR} :::"
+echo ":::::::::::::::::::::::::"
+echo -e "\n"
 
 # Check output for Fixes
 csdiff --fixed "../dest-br-shellcheck.err" "../pr-br-shellcheck.err" > ../fixes.log
 if [ "$(cat ../fixes.log | wc -l)" -ne 0 ]; then
-  echo "Fixed bugs:" 
+  echo -e "${GREEN}Fixed bugs:${NOCOLOR}" 
   csgrep ../fixes.log
-  echo "------------"
+  echo "---------------------"
 else
-  echo "No Fixes!"
-  echo "------------"
+  echo -e "${YELLOW}No Fixes!${NOCOLOR}"
+  echo "---------------------"
 fi
+echo -e "\n"
 
 # Check output for added bugs
 csdiff --fixed "../pr-br-shellcheck.err" "../dest-br-shellcheck.err" > ../bugs.log
 if [ "$(cat ../bugs.log | wc -l)" -ne 0 ]; then
-  echo "Added bugs, NEED INSPECTION:" 
+  echo -e "${RED}Added bugs, NEED INSPECTION:${NOCOLOR}" 
   csgrep ../bugs.log
-  echo "------------"
+  echo "---------------------"
   exitstatus=1
 else
-  echo "No bugs added Yay!" 
-  echo "------------"
+  echo -e "${GREEN}No bugs added Yay!${NOCOLOR}" 
+  echo "---------------------"
   exitstatus=0
 fi
 
 exit $exitstatus
-
